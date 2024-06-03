7c0774f9eec60337d5cda11573759f5ca9d5b02f
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-06-03 18:19:02 -0300

    validating blank field

--- src/main/java/com/eletronicos/jfctecnologia/eletronico/infra/TratadorDeErros.java
+++ src/main/java/com/eletronicos/jfctecnologia/eletronico/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package com.eletronicos.jfctecnologia.eletronico.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,21 @@ public class TratadorDeErros {
 	public ResponseEntity<?> tratador404() {
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> tratador400 (MethodArgumentNotValidException ex){
+		
+		var erros = ex.getFieldErrors();
+		
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+	}
+	
+	public record DadosErros(String field, String message) {
+		
+		public DadosErros(FieldError erro) {
+			this(erro.getField(),erro.getDefaultMessage());
+		}
 	}
 }
